6940bc12c2b88aee1fb7bb4427d51dc9389a4f76
Mamun-Developer <hridoy.developer@gmail.com>
2021-04-20 02:44:52 +0600

    dbms installed
    Signed-off-by: Mamun-Developer <hridoy.developer@gmail.com>

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from userData import UserData
+from firebase_admin import db
 
 user1 = UserData(name=input("Name: "), email=input("Email: "), phone=input("Phone: "), age=input("Age: "))
+if db._http_client == 1:
+    print("")
 
 print(user1.get_name())
 print(user1.get_email())
